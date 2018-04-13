2bd1f685883d3f684ee6276fe60ae6f12db13f36
Leo <leo.kwok@beamly.com>
2018-04-13 21:34:14 +0100

    tidy up and add nodemon

--- 02-Node_js-Fundamentals-Note-App/notes-node/app.js
+++ 02-Node_js-Fundamentals-Note-App/notes-node/app.js
@@ -1,18 +1,6 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
-
-// console.log(_.isString(true));
-// console.log(_.isString('Andrew'));
-
-var filteredArray = _.uniq(['Leo', 1, 'Leo', 1, 2, 3, 4]);
-console.log(filteredArray);
-
-// console.log('Result:', notes.add(9, -2));
-
-// var user = os.userInfo();
-
-// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
