564c28a38fc9f93df8db006d7b7d62524465ea36
Leo <leo.kwok@beamly.com>
2018-04-13 20:32:49 +0100

    using require

---
+++ 02-Node_js-Fundamentals-Note-App/notes-node/app.js
@@ -0,0 +1,12 @@
+console.log('Starting app.js');
+
+const fs = require('fs');
+const os = require('os');
+const notes = require('./notes.js');
+
+
+console.log('Result:', notes.add(9, -2));
+
+// var user = os.userInfo();
+
+// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
