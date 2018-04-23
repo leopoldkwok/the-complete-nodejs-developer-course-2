5bf20bc36a4d8139f61722ee0b718d668dbce6b7
Leo <leo.kwok@beamly.com>
2018-04-23 11:39:31 +0100

    Wiring Up Weather Search

--- 03-Asynchronous-Node_js-Weather-App/app.js
+++ 03-Asynchronous-Node_js-Weather-App/app.js
@@ -1,24 +1,41 @@
-const yargs = require('yargs');
+// const yargs = require('yargs');
 
-const geocode = require('./geocode/geocode');
+// const geocode = require('./geocode/geocode');
 
-const argv = yargs
-    .options({
-        a: {
-            demand: true,
-            alias: 'address',
-            describe: 'Address to fetch weather for',
-            string: true
-        }
-    })
-    .help()
-    .alias('help', 'h')
-    .argv;
+// const argv = yargs
+//     .options({
+//         a: {
+//             demand: true,
+//             alias: 'address',
+//             describe: 'Address to fetch weather for',
+//             string: true
+//         }
+//     })
+//     .help()
+//     .alias('help', 'h')
+//     .argv;
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-    if (errorMessage) {
-        console.log(errorMessage);
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//     if (errorMessage) {
+//         console.log(errorMessage);
+//     } else {
+//         console.log(JSON.stringify(results, undefined, 2));
+//     }
+// });
+
+
+// 5750238265d2e259edf3320bc4b94977
+
+const request = require('request');
+
+request({
+    url: 'https://api.darksky.net/forecast/5750238265d2e259edf3320bc4b94977/39.9396284,-75.18663959999999',
+    json: true
+}, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+        console.log(body.currently.temperature);
     } else {
-        console.log(JSON.stringify(results, undefined, 2));
+        console.log('Unable to fetch weather.');
     }
+
 });
