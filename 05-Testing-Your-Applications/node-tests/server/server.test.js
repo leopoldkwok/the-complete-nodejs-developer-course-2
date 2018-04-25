a90ad30d1dff1c18fef5d56a10ed88ea225161d1
Leo <leo.kwok@beamly.com>
2018-04-25 23:10:44 +0100

    Organizing Test With describe()

--- 05-Testing-Your-Applications/node-tests/server/server.test.js
+++ 05-Testing-Your-Applications/node-tests/server/server.test.js
@@ -3,31 +3,46 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return hello world response', (done) => {
-    request(app)
-        .get('/')
-        .expect(404)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                error: 'Page not found.'
-            });
-        })
-        .end(done);
+// server
+// GET /
+// some test case
+// Get /users
+// some test case
+
+describe('Server', () => {
+    describe('GET /', () => {
+        it('should return hello world response', (done) => {
+            request(app)
+                .get('/')
+                .expect(404)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        error: 'Page not found.'
+                    });
+                })
+                .end(done);
+        });
+    });
+
+    describe('GET /users', () => {
+        // Make a new test
+        // assert 200
+        // Assert that you exist in users array
+
+        it('should return my user object', (done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        name: 'Andrew',
+                        age: 25
+                    });
+                })
+                .end(done);
+        });
+    });
 });
 
-// Make a new test
-// assert 200
-// Assert that you exist in users array
 
-it('should return my user object', (done) => {
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                name: 'Andrew',
-                age: 25
-            });
-        })
-        .end(done);
-});
+
--- 05-Testing-Your-Applications/node-tests/utils/utils.test.js
+++ 05-Testing-Your-Applications/node-tests/utils/utils.test.js
@@ -2,31 +2,39 @@ const expect = require('expect');
 
 const utils = require('./utils');
 
-it('it should add two numbers', () => {
-    var res = utils.add(33, 11);
+describe('Utils', () => {
 
-    expect(res).toBe(44).toBeA('number');
-});
+    describe('#add', () => {
+        it('it should add two numbers', () => {
+            var res = utils.add(33, 11);
 
-it('should async add two numbers', (done) => {
-    utils.asyncAdd(4, 3, (sum) => {
-        expect(sum).toBe(7).toBeA('number');
-        done();
+            expect(res).toBe(44).toBeA('number');
+        });
+    });
+
+
+    it('should async add two numbers', (done) => {
+        utils.asyncAdd(4, 3, (sum) => {
+            expect(sum).toBe(7).toBeA('number');
+            done();
+        });
+    });
+
+    it('it should square a number', () => {
+        var res = utils.square(3);
+
+        expect(res).toBe(9).toBeA('number');
+    });
+
+    it('should async square a number', (done) => {
+        utils.asyncSquare(5, (res) => {
+            expect(res).toBe(25).toBeA('number');
+            done();
+        })
     });
 });
 
-it('it should square a number', () => {
-    var res = utils.square(3);
 
-    expect(res).toBe(9).toBeA('number');
-});
-
-it('should async square a number', (done) => {
-    utils.asyncSquare(5, (res) => {
-        expect(res).toBe(25).toBeA('number');
-        done();
-    })
-});
 
 // should verify first and last names are set
 // assert it includes firstName and lastName with proper values
