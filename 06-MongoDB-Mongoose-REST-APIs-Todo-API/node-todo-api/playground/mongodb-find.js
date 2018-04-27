b10adb6a2a150d4fc407c4710692f9ed97df66c1
Leo <leo.kwok@beamly.com>
2018-04-27 19:32:02 +0100

    Fetching Data

---
+++ 06-MongoDB-Mongoose-REST-APIs-Todo-API/node-todo-api/playground/mongodb-find.js
@@ -0,0 +1,30 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectId} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectId('5ae368aecdd6710ab280163c')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    db.collection('Users').find({name: 'Andrew'}).toArray().then((docs) => {
+        console.log(JSON.stringify(docs, undefined, 2));
+    });
+
+    // db.close();
+});
